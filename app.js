3f447d948bb182c373343bf3d46c9e1fcc4386fc
Srikanth Munaga <Srikanth_Munaga@epam.com>
2019-06-26 15:55:53 +0530

    chaged the variable name from pro to products

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { User, Products } from "./models";
 import * as config from "./config/config.json";
-var user = new User();
-var pro = new Products();
 console.log(config.appName);
+var user = new User();
+var products = new Products();
