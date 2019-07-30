03844c20500f3afc021e7db09be6203432ac7b55
srikanth munaga <srikanth.cse527@gmail.com>
2019-07-30 16:57:07 +0530

    Update AuthError.js

--- express-server/helpers/AuthError.js
+++ express-server/helpers/AuthError.js
@@ -1,6 +1,5 @@
 const failedAuth = function(name, message) {
-  console.log("here");
-  return name + "Authentication failed";
+    return name + "Authentication failed";
 };
 
 export { failedAuth };
